bdc7863b1b2419ef7ca7b3d900292bd598439de6
Leah Rapp <leah.rapp@gmail.com>
2018-04-01 18:18:04 -0400

    Add files via upload

---
+++ Implimenting_Ajax_Rapp_Leah/main/apps/ajax/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
+from models import *
+from django.core import serializers
+from django.http import JsonResponse
+import json
+
+# Create your views here.
+
+def index(request):
+    
+    #return render(request, "ajax/index.html" )
+   
+    if "note" in request.POST:
+        data=request.POST["note"]
+        print(data)
+        return HttpResponse(json.dumps(data), content_type="application/json" )
+    else:
+        return render(request, "ajax/index.html" )
+
+
