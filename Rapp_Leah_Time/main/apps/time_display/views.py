9048173cb4e8d6438caed6f5d8a3ba4d955beefb
Leah Rapp <leah.rapp@gmail.com>
2018-04-01 17:33:28 -0400

    Add files via upload

---
+++ Rapp_Leah_Time/main/apps/time_display/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, HttpResponse, redirect
+
+# Create your views here.
+import time
+from datetime import datetime 
+def index(request):
+    context = {
+        "time" : datetime.now().strftime("%Y-%m-%d %H:%M %p")
+    }
+    return render(request, "time_display/index.html", context)
