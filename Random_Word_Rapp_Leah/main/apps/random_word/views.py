c5f6d480fe28b3d1d0e49084967a271da341118d
Leah Rapp <leah.rapp@gmail.com>
2018-04-01 17:36:29 -0400

    Add files via upload

---
+++ Random_Word_Rapp_Leah/main/apps/random_word/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.utils.crypto import get_random_string
+# Create your views here.
+import time
+from datetime import datetime 
+
+def index(request):
+    if 'counter' not in request.session:
+        request.session['counter']=0
+    request.session['counter']+=1
+    
+    
+    word=get_random_string(length=14)
+    request.session["word"]=word
+    return render(request, "random_word/index.html")
